106d6ac3d0b95c9debab39008c7d3784d490f74c
Ensar <ensar_mehmedovic@hotmail.com>
2025-08-08 14:41:40 +0200

    commit

--- prem-players/src/About.jsx
+++ prem-players/src/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import lion from './assets/lion.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+            <img src={lion} alt="lion"/>
+            <h1>Welcome to PremStats</h1>
+            <button>Start now</button>
+    </div>
   )
 }
 
--- prem-players/src/App.jsx
+++ prem-players/src/App.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import './App.css'
+import './index.css'
 import About from "./About";
 import Home from "./Home";
 
